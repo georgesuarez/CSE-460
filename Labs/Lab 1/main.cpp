b12898eed7fa78915ae79b5c5dbbfa23c3abcce9
georgesuarez <georgejsuarez@gmail.com>
2018-04-15 11:11:38 -0700

    Made a Labs folder and moved Lab 1 into Labs

---
+++ Labs/Lab 1/main.cpp
@@ -0,0 +1,7 @@
+#include "message.h"
+
+int main()
+{
+    display_message("CSUSB CSE 460 is Beautiful!");
+    return 0;
+}
---
+++ Labs/Lab 1/message.cpp
@@ -0,0 +1,8 @@
+#include "message.h"
+#include <string>
+#include <iostream>
+
+void display_message(std::string msg)
+{
+    std::cout << msg << std::endl;
+}
