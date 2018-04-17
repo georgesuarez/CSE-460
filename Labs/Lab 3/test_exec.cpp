2dcdf4184b95571f47da20ec0daa97236fbf6f4c
George Suarez <georgejsuarez@gmail.com>
2018-04-16 17:59:33 -0700

    Added a Labs folder

---
+++ Labs/Lab 3/test_exec.cpp
@@ -0,0 +1,15 @@
+//test_exec.cpp
+#include <unistd.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Running ps with execlp\n" ;
+  execl( "ps", "ps", "-ax", 0 );
+
+  cout << "Done!\n";
+  
+  return 0;
+}
